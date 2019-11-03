d7871a13c7c3db6cfff8cf41bcaffd8147612a7e
nacho <nacho@DESKTOP-L3GCH1I>
2019-11-03 18:29:51 -0300

    Agregados comentarios

--- src/main/java/ar/edu/uner/fcad/ed/mapeosdiccionarios/mapeos/MapeoInterfaz.java
+++ src/main/java/ar/edu/uner/fcad/ed/mapeosdiccionarios/mapeos/MapeoInterfaz.java
@@ -14,24 +14,72 @@ import ar.edu.uner.fcad.ed.mapeosdiccionarios.conjuntos.ConjuntoInterfaz;
  * @param <K>
  * @param <V>
  */
-public interface MapeoInterfaz <K, V>{
+public interface MapeoInterfaz<K, V> {
 
+    /**
+     * Indica la cantidad de entradas que hay en el mapeo.
+     *
+     * @return
+     */
     public int size();
 
+    /**
+     * Indica si la estructura está vacía.
+     *
+     * @return
+     */
     public boolean isEmpty();
-    
+
+    /**
+     * Devuelve el valor asociado a key.
+     *
+     * @param key
+     * @return
+     */
     public V get(K key);
-    
+
+    /**
+     * Inserta una nueva entrada clave key y valor value.
+     *
+     * @param key
+     * @param value
+     * @return
+     */
     public V put(K key, V value);
-    
+
+    /**
+     * Remueve la entrada con clave key
+     *
+     * @param key
+     * @return
+     * @throws Exception
+     */
     public V remove(K key) throws Exception;
-    
+
+    /**
+     * Devuelve un conjunto con todas las claves del mapeo.
+     *
+     * @return
+     */
     public ConjuntoInterfaz<K> keySet();
-    
+
+    /**
+     * Devuelve todos las valores del mapeo.
+     *
+     * @return
+     */
     public ConjuntoInterfaz<V> values();
-    
+
+    /**
+     * Devuelve un conjunto con todas las entradas del mapeo.
+     * @return 
+     */
     public ConjuntoInterfaz<Entrada<K, V>> entrySet();
-    
+
+    /**
+     * Concatena en un String el estado actual de la estructura.
+     * @return 
+     */
     @Override
     public String toString();
 }
